5d0aefeba56861a1db797ffb0472da39c8e8bdf6
kaczkifajne <113357260+kaczkifajne@users.noreply.github.com>
2026-03-12 09:24:39 +0100

    Update PlayerControllerX.cs

--- challange1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ challange1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -17,14 +17,14 @@ public class PlayerControllerX : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void LateUpdate()
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
         arrowInput = Input.GetAxis("Horizontal");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward * speed * verticalInput);
+        transform.Translate(Vector3.forward * speed * verticalInput * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
         transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * arrowInput);
