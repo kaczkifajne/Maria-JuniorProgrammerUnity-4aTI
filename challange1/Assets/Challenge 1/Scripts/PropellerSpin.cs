12c44a24c2bde314059fe694a1a6dca6751859d5
kaczkifajne <113357260+kaczkifajne@users.noreply.github.com>
2026-03-12 09:24:19 +0100

    poprawki i dodanie animacji
    - poprawki dotyczace ruchu gracza (zmiana funkcji z FixedUpdate na LateUpdate i dodanie deltaTime)
    - smiglo sie kreci brbrbr

---
+++ challange1/Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    public Vector3 spinRate = new Vector3(0, 0, 4);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(spinRate);
+    }
+}
